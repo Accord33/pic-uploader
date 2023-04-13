0870c2f7d2b8abd1a06e2508b9f0d5d5b35a76b9
Staycia930 <83445687+Accord33@users.noreply.github.com>
2023-04-13 16:01:36 +0900

    4/13

--- show.php
+++ show.php
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/828286173700816947/976693328487792651/3_20220519125016.png">
+</head>
+<body>
 <?php
 // picフォルダの中のjpgファイルを取得する
 $files = glob("pic/*");
 
 // 画像を表示する
 foreach ($files as $file) {
-    echo "<img style='max-width:50%;height:auto;' src='$file' alt='pic'>";
+    echo "<img style='width:20%;height:auto;' src='$file' alt='pic'>";
 }
 ?>
+</body>
+</html>
